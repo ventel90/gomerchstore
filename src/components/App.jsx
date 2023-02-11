76dba318794bc5b51ed60c1e0258195a20345992
Vitaliy Guivan <107477405+ventel90@users.noreply.github.com>
2023-02-11 20:50:49 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import { SharedLayout } from "./SharedLayout";
+
+const About = lazy(() => import("../pages/About"));
+const Home = lazy(() => import("../pages/Home"));
+const ProductDetails = lazy(() => import("../pages/ProductDetails"));
+const Products = lazy(() => import("../pages/Products"));
+const Mission = lazy(() => import("./Mission"));
+const Team = lazy(() => import("./Team"));
+const Reviews = lazy(() => import("./Reviews"));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+      </Route>
+    </Routes>
+  );
+};
